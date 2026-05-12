ed94d312dcfbcea97e189034612b6db3e81674ff
Progga18 <ppa220000@utdallas.edu>
2026-05-11 21:15:28 -0500

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,20 +1,17 @@
-function flipCoin() {
-  const coin = document.getElementById("coin");
-  coin.classList.toggle("flip");
-}
-
 function handleChoice(choice) {
   const coin = document.getElementById("coin");
 
-  coin.classList.add("flip");
+  coin.classList.toggle("flip");
 
   let message = "";
 
   if (choice === "let go") {
     message = "What I release no longer controls me.";
-  } else if (choice === "understand") {
+  } 
+  else if (choice === "understand") {
     message = "My fear became smaller when I looked closely at it.";
-  } else if (choice === "face it") {
+  } 
+  else if (choice === "face it") {
     message = "My fear seemed less powerful when I decided to face it.";
   }
 
